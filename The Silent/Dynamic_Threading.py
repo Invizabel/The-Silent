225d5e85977e3fcab28e806cfe1341c9c5c301b0
Invizabel <46697708+Invizabel@users.noreply.github.com>
2021-12-25 21:47:39 -0600

    Delete Dynamic_Threading.py

--- The Silent/Dynamic_Threading.py
+++
@@ -1,9 +0,0 @@
-#"code" must be a string
-def dthread(code):
-    try:
-        hyper_thread = threading.Thread(target = code)
-        hyper_thread.start()
-        threads.append(hyper_thread)
-
-    except:
-        pass
