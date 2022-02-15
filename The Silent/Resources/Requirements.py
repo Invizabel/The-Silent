6645bf0f5073396fb453c79fdc23a6b6e4d1d90c
Invizabel <46697708+Invizabel@users.noreply.github.com>
2022-02-14 20:16:27 -0600

    Add files via upload

--- The Silent/Resources/Requirements.py
+++ The Silent/Resources/Requirements.py
@@ -3,6 +3,7 @@ import os
 #twint modules
 os.system("pip install aiohttp_socks")
 os.system("pip install bs4")
+os.system("pip3 install . -r twint\ requirements.txt")
 
 os.system("pip install requests")
 os.system("pip install pysocks")
