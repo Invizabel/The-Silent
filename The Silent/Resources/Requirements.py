6540888748ee25cf83e7f35805bc01de65f40709
Invizabel <46697708+Invizabel@users.noreply.github.com>
2022-04-21 12:54:18 -0500

    Update Requirements.py

--- The Silent/Resources/Requirements.py
+++ The Silent/Resources/Requirements.py
@@ -1,17 +1,17 @@
 import os
 
 #twint modules
-os.system("pip install aiohttp==3.7.0")
-os.system("pip install aiohttp_socks")
-os.system("pip install bs4")
+os.system("pip3 install aiohttp==3.7.0")
+os.system("pip3 install aiohttp_socks")
+os.system("pip3 install bs4")
 os.system("pip3 install -r twint.txt")
 
-os.system("pip install BeautifulSoup")
-os.system("pip install webdriver-manager")
-os.system("pip install selenium")
+os.system("pip3 install BeautifulSoup")
+os.system("pip3 install webdriver-manager")
+os.system("pip3 install selenium")
+os.system("pip3 install PySocks")
 
-os.system("pip install matplotlib")
-os.system("pip install requests")
-os.system("pip install pysocks")
-os.system("pip install imagehash")
-
+os.system("pip3 install matplotlib")
+os.system("pip3 install requests")
+os.system("pip3 install pysocks")
+os.system("pip3 install imagehash")
