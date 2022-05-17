89fb672b971064849769849c075600792f240971
Invizabel <46697708+Invizabel@users.noreply.github.com>
2022-05-17 16:30:41 -0500

    Add files via upload

--- The Silent/Resources/Termux Requirements.py
+++ The Silent/Resources/Termux Requirements.py
@@ -1,19 +1,25 @@
 import os
 
 os.system("apt update")
+os.system("apt upgrade")
 os.system("apt install termux-services")
 os.system("apt install tor")
+os.system("apt install libjpeg-turbo")
+
+os.system("pip3 install wheel")
 
 #twint modules
-os.system("pip install aiohttp==3.7.0")
-os.system("pip install aiohttp_socks")
-os.system("pip install bs4")
+os.system("pip3 install aiohttp==3.7.0")
+os.system("pip3 install aiohttp_socks")
+os.system("pip3 install bs4")
 os.system("pip3 install -r twint.txt")
 
-os.system("pip install requests")
-os.system("pip install pysocks")
-os.system("pip install BeautifulSoup")
+os.system("pip3 install requests")
+os.system("pip3 install pysocks")
+os.system("pip3 install BeautifulSoup")
 
-os.system("pip install matplotlib")
-os.system("pip install pandas")
-os.system("pip install imagehash")
+os.system("pip3 install matplotlib")
+os.system("pip3 install pandas")
+os.system("pip3 install imagehash")
+
+os.system("pip3 install pillow")
