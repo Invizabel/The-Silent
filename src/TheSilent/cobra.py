ec598db9671be665c4ef602003dd7b21871a9179
Invizabel <46697708+Invizabel@users.noreply.github.com>
2024-02-05 21:08:43 -0600

    Add files via upload

--- src/TheSilent/cobra.py
+++ src/TheSilent/cobra.py
@@ -2,7 +2,6 @@ import re
 import time
 import urllib.parse
 from TheSilent.clear import clear
-from TheSilent.dolphin import dolphin
 from TheSilent.kitten_crawler import kitten_crawler
 from TheSilent.puppy_requests import text
 
@@ -15,20 +14,36 @@ def cobra(host,delay=0,crawl=1):
     
     hits = []
 
-    mal_python = [r"time.sleep(60)",
-                  r"eval(compile('import time\ntime.sleep(60)','cobra','exec'))",
-                  r"eval(compile('import os\nos.system('sleep 60')','cobra','exec'))",
-                  r"__import__('time').sleep(60)",
-                  r"__import__('os').system('sleep 60')",
-                  r'eval("__import__(\'time\').sleep(60)")',
-                  r'eval("__import__(\'os\').system(\'sleep 60\')")',
-                  r'exec("__import__(\'time\').sleep(60)")',
-                  r'exec("__import__(\'os\').system(\'sleep 60\')")',
-                  r'exec("import time\ntime.sleep(60)")',
-                  r'exec("import os\nos.system(\'sleep 60\')")']
+    mal_command = [r"sleep 30"]
+
+    mal_directory = [r"/admin",
+                     r"/login",
+                     r"/signup"]
+
+    mal_php = [r"sleep(30)",
+               r"sleep(30);"]
+
+    mal_python = [r"time.sleep(30)",
+                  r"eval(compile('import time\ntime.sleep(30)','cobra','exec'))",
+                  r"eval(compile('import os\nos.system('sleep 30')','cobra','exec'))",
+                  r"__import__('time').sleep(30)",
+                  r"__import__('os').system('sleep 30')",
+                  r'eval("__import__(\'time\').sleep(30)")',
+                  r'eval("__import__(\'os\').system(\'sleep 30\')")',
+                  r'exec("__import__(\'time\').sleep(30)")',
+                  r'exec("__import__(\'os\').system(\'sleep 30\')")',
+                  r'exec("import time\ntime.sleep(30)")',
+                  r'exec("import os\nos.system(\'sleep 30\')")']
     
     mal_subdomain = ["ad",
+                     "admin",
+                     "byod",
+                     "camera",
+                     "cameras",
+                     "cctv",
                      "dev",
+                     "extranet",
+                     "git",
                      "hr",
                      "internal",
                      "intranet",
@@ -36,12 +51,16 @@ def cobra(host,delay=0,crawl=1):
                      "lan",
                      "ldap",
                      "local",
-                     "test"]
+                     "private",
+                     "secret",
+                     "secrets",
+                     "test",
+                     "wan"]
     
     mal_xss = [r"<iframe>Cobra</iframe>",
                r"<p>cobra</p>",
                r"<script>alert('Cobra')</script>",
-               r"<script>await sleep(60);</script>",
+               r"<script>await sleep(30);</script>",
                r"<script>prompt('Cobra')</script>",
                r"<strong>cobra</strong>",
                r"<style>body{background-color:red;}</style>",
@@ -51,16 +70,34 @@ def cobra(host,delay=0,crawl=1):
     for mal in mal_subdomain:
         time.sleep(delay)
         try:
-            print(f"checking: " + mal + "." + ".".join(urllib.parse.urlparse(host).netloc.split('.')[-2:]))
-            ports = dolphin(mal + "." + ".".join(urllib.parse.urlparse(host).netloc.split('.')[-2:]))
-            for port in ports:
-                hits.append("found sensitive subdomain: " + mal + "." + ".".join(urllib.parse.urlparse(host).netloc.split('.')[-2:]) + f":{port}")
+            print(f"checking: http://" + mal + "." + ".".join(urllib.parse.urlparse(host).netloc.split('.')[-2:]))
+            text("http://" + mal + "." + ".".join(urllib.parse.urlparse(host).netloc.split('.')[-2:]))
+            hits.append("found sensitive subdomain: http://" + mal + "." + ".".join(urllib.parse.urlparse(host).netloc.split('.')[-2:]))
 
-        except PermissionError:
+        except:
+            pass
+
+        try:
+            print(f"checking: https://" + mal + "." + ".".join(urllib.parse.urlparse(host).netloc.split('.')[-2:]))
+            text("https://" + mal + "." + ".".join(urllib.parse.urlparse(host).netloc.split('.')[-2:]))
+            hits.append("found sensitive subdomain: https://" + mal + "." + ".".join(urllib.parse.urlparse(host).netloc.split('.')[-2:]))
+
+        except:
             pass
 
     print(CYAN + f"crawling: {host}")
     hosts = kitten_crawler(host,delay,crawl)
+
+    for mal in mal_directory:
+        time.sleep(delay)
+        try:
+            print(CYAN + host.rstrip("/") + mal)
+            text(host.rstrip("/") + mal)
+            hosts.append(host.rstrip("/") + mal)
+        except:
+            pass
+
+    hosts = list(dict.fromkeys(hosts[:]))
     clear()
     for _ in hosts:
         if urllib.parse.urlparse(host).netloc in urllib.parse.urlparse(_).netloc:
@@ -71,6 +108,102 @@ def cobra(host,delay=0,crawl=1):
             except:
                 forms = []
 
+            # check for command injection
+            for mal in mal_command:
+                print(CYAN + f"checking {_} with payload {mal}")
+                try:
+                    time.sleep(delay)
+                    start = time.time()
+                    data = text(_ + "/" + mal, timeout = 120)
+                    end = time.time()
+                    if end - start >= 25:
+                        hits.append(f"command injection in url: {_}/{mal}")
+
+                except:
+                    pass
+
+                try:
+                    time.sleep(delay)
+                    start = time.time()
+                    data = text(_, headers = {"Cookie",mal}, timeout = 120)
+                    end = time.time()
+                    if end - start >= 25:
+                        hits.append(f"command injection in cookie ({mal}): {_}")
+
+                except:
+                    pass
+
+                try:
+                    time.sleep(delay)
+                    start = time.time()
+                    data = text(_, headers = {"Referer",mal}, timeout = 120)
+                    end = time.time()
+                    if end - start >= 25:
+                        hits.append(f"command injection in referer ({mal}): {_}")
+
+                except:
+                    pass
+                
+                for form in forms:
+                    field_list = []
+                    input_field = re.findall("<input.+?>",form)
+                    try:
+                        action_field = re.findall("action\s*=\s*[\"\'](\S+)[\"\']",form)[0]
+                        if action_field.startswith("/"):
+                            action = _ + action_field
+
+                        elif not action_field.startswith("/") and not action_field.startswith("http://") and not action_field.startswith("https://"):
+                            action = _ + "/" + action_field
+
+                        else:
+                            action = action_field
+                            
+                    except IndexError:
+                        pass
+
+                    try:
+                        method_field = re.findall("method\s*=\s*[\"\'](\S+)[\"\']",form)[0].upper()
+                        for in_field in input_field:
+                            if re.search("name\s*=\s*[\"\'](\S+)[\"\']",in_field) and re.search("type\s*=\s*[\"\'](\S+)[\"\']",in_field):
+                                name_field = re.findall("name\s*=\s*[\"\'](\S+)[\"\']",in_field)[0]
+                                type_field = re.findall("type\s*=\s*[\"\'](\S+)[\"\']",in_field)[0]
+                                
+                                try:
+                                    value_field = re.findall("value\s*=\s*[\"\'](\S+)[\"\']",in_field)[0]
+                                
+                                except IndexError:
+                                    value_field = ""
+                                
+                                if type_field == "submit" or type_field == "hidden":
+                                    field_list.append({name_field:value_field})
+
+
+                                if type_field != "submit" and type_field != "hidden":
+                                    field_list.append({name_field:mal})
+
+                                field_dict = field_list[0]
+                                for init_field_dict in field_list[1:]:
+                                    field_dict.update(init_field_dict)
+
+                                time.sleep(delay)
+
+                                if action:
+                                    start = time.time()
+                                    data = text(action,method=method_field,data=field_dict,timeout=120)
+                                    end = time.time()
+                                    if end - start >= 25:
+                                        hits.append(f"command injection in forms: {action} | {field_dict}")
+
+                                else:
+                                    start = time.time()
+                                    data = text(action,method=method_field,data=field_dict,timeout=120)
+                                    end = time.time()
+                                    if end - start >= 25:
+                                        hits.append(f"command injection in forms: {_} | {field_dict}")
+
+                    except:
+                        pass
+
             # check for python injection
             for mal in mal_python:
                 print(CYAN + f"checking {_} with payload {mal}")
@@ -79,7 +212,7 @@ def cobra(host,delay=0,crawl=1):
                     start = time.time()
                     data = text(_ + "/" + mal, timeout = 120)
                     end = time.time()
-                    if end - start >= 45:
+                    if end - start >= 25:
                         hits.append(f"python injection in url: {_}/{mal}")
 
                 except:
@@ -90,7 +223,7 @@ def cobra(host,delay=0,crawl=1):
                     start = time.time()
                     data = text(_, headers = {"Cookie",mal}, timeout = 120)
                     end = time.time()
-                    if end - start >= 45:
+                    if end - start >= 25:
                         hits.append(f"python injection in cookie ({mal}): {_}")
 
                 except:
@@ -101,7 +234,7 @@ def cobra(host,delay=0,crawl=1):
                     start = time.time()
                     data = text(_, headers = {"Referer",mal}, timeout = 120)
                     end = time.time()
-                    if end - start >= 45:
+                    if end - start >= 25:
                         hits.append(f"python injection in referer ({mal}): {_}")
 
                 except:
@@ -154,15 +287,111 @@ def cobra(host,delay=0,crawl=1):
                                     start = time.time()
                                     data = text(action,method=method_field,data=field_dict,timeout=120)
                                     end = time.time()
-                                    if end - start >= 45:
+                                    if end - start >= 25:
                                         hits.append(f"python injection in forms: {action} | {field_dict}")
 
                                 else:
                                     start = time.time()
                                     data = text(action,method=method_field,data=field_dict,timeout=120)
                                     end = time.time()
-                                    if end - start >= 45:
+                                    if end - start >= 25:
                                         hits.append(f"python injection in forms: {_} | {field_dict}")
+
+                    except:
+                        pass
+
+            # check for php injection
+            for mal in mal_php:
+                print(CYAN + f"checking {_} with payload {mal}")
+                try:
+                    time.sleep(delay)
+                    start = time.time()
+                    data = text(_ + "/" + mal, timeout = 120)
+                    end = time.time()
+                    if end - start >= 25:
+                        hits.append(f"php injection in url: {_}/{mal}")
+
+                except:
+                    pass
+
+                try:
+                    time.sleep(delay)
+                    start = time.time()
+                    data = text(_, headers = {"Cookie",mal}, timeout = 120)
+                    end = time.time()
+                    if end - start >= 25:
+                        hits.append(f"php injection in cookie ({mal}): {_}")
+
+                except:
+                    pass
+
+                try:
+                    time.sleep(delay)
+                    start = time.time()
+                    data = text(_, headers = {"Referer",mal}, timeout = 120)
+                    end = time.time()
+                    if end - start >= 25:
+                        hits.append(f"php injection in referer ({mal}): {_}")
+
+                except:
+                    pass
+                
+                for form in forms:
+                    field_list = []
+                    input_field = re.findall("<input.+?>",form)
+                    try:
+                        action_field = re.findall("action\s*=\s*[\"\'](\S+)[\"\']",form)[0]
+                        if action_field.startswith("/"):
+                            action = _ + action_field
+
+                        elif not action_field.startswith("/") and not action_field.startswith("http://") and not action_field.startswith("https://"):
+                            action = _ + "/" + action_field
+
+                        else:
+                            action = action_field
+                            
+                    except IndexError:
+                        pass
+
+                    try:
+                        method_field = re.findall("method\s*=\s*[\"\'](\S+)[\"\']",form)[0].upper()
+                        for in_field in input_field:
+                            if re.search("name\s*=\s*[\"\'](\S+)[\"\']",in_field) and re.search("type\s*=\s*[\"\'](\S+)[\"\']",in_field):
+                                name_field = re.findall("name\s*=\s*[\"\'](\S+)[\"\']",in_field)[0]
+                                type_field = re.findall("type\s*=\s*[\"\'](\S+)[\"\']",in_field)[0]
+                                
+                                try:
+                                    value_field = re.findall("value\s*=\s*[\"\'](\S+)[\"\']",in_field)[0]
+                                
+                                except IndexError:
+                                    value_field = ""
+                                
+                                if type_field == "submit" or type_field == "hidden":
+                                    field_list.append({name_field:value_field})
+
+
+                                if type_field != "submit" and type_field != "hidden":
+                                    field_list.append({name_field:mal})
+
+                                field_dict = field_list[0]
+                                for init_field_dict in field_list[1:]:
+                                    field_dict.update(init_field_dict)
+
+                                time.sleep(delay)
+
+                                if action:
+                                    start = time.time()
+                                    data = text(action,method=method_field,data=field_dict,timeout=120)
+                                    end = time.time()
+                                    if end - start >= 25:
+                                        hits.append(f"php injection in forms: {action} | {field_dict}")
+
+                                else:
+                                    start = time.time()
+                                    data = text(action,method=method_field,data=field_dict,timeout=120)
+                                    end = time.time()
+                                    if end - start >= 25:
+                                        hits.append(f"php injection in forms: {_} | {field_dict}")
 
                     except:
                         pass
