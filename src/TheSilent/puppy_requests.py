190d864baf826b5739c4a148f9c0ac4f684c8a35
Invizabel <46697708+Invizabel@users.noreply.github.com>
2024-03-20 12:16:58 -0500

    Add files via upload
    updated puppy_requests and cobra

--- src/TheSilent/puppy_requests.py
+++ src/TheSilent/puppy_requests.py
@@ -1,3 +1,4 @@
+import http.cookiejar
 import ssl
 import urllib.parse
 import urllib.request
@@ -10,26 +11,45 @@ fake_headers = {"Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,
                 "User-Agent":return_user_agent(),
                 "UPGRADE-INSECURE-REQUESTS":"1"}
 
+# Create a CookieJar to store cookies
+cookie_jar = http.cookiejar.CookieJar()
+cookie_handler = urllib.request.HTTPCookieProcessor(cookie_jar)
+opener = urllib.request.build_opener(cookie_handler)
+
 def getheaders(host,method="GET",data=b"",headers=fake_headers,timeout=10):
-    simple_request = urllib.request.Request(host,data=urllib.parse.urlencode(data).encode(),method=method,unverifiable=True)
-    for _,__ in headers.items():
-        simple_request.add_header(_,__)
-    simple_response = urllib.request.urlopen(simple_request,timeout=timeout)
+    simple_request = urllib.request.Request(host,data=urllib.parse.urlencode(data).encode(),method=method.upper(),unverifiable=True)
+    for key,value in fake_headers.items():
+        simple_request.add_header(key,value)
+
+    for key,value in headers.items():
+        simple_request.add_header(key,value)
+
+    simple_response = opener.open(simple_request,timeout=timeout)
+
     return simple_response.headers
 
-def text(host,method="GET",data=b"",headers=fake_headers,timeout=10,raw=False):
-    simple_request = urllib.request.Request(host,data=urllib.parse.urlencode(data).encode(),method=method,unverifiable=True)
-    for _,__ in headers.items():
-        simple_request.add_header(_,__)
-    simple_response = urllib.request.urlopen(simple_request,timeout=timeout)
+def text(host,method="GET",data=b"",headers={},timeout=10,raw=False):
+    simple_request = urllib.request.Request(host,data=urllib.parse.urlencode(data).encode(),method=method.upper(),unverifiable=True)
+    for key,value in fake_headers.items():
+        simple_request.add_header(key,value)
+
+    for key,value in headers.items():
+        simple_request.add_header(key,value)
+
+    simple_response = opener.open(simple_request,timeout=timeout)
     if raw:
         return simple_response.read()
     else:
         return simple_response.read().decode("ascii",errors="ignore")
 
 def url(host,method="GET",data=b"",headers=fake_headers,timeout=10):
-    simple_request = urllib.request.Request(host,data=urllib.parse.urlencode(data).encode(),method=method,unverifiable=True)
-    for _,__ in headers.items():
-        simple_request.add_header(_,__)
-    simple_response = urllib.request.urlopen(simple_request,timeout=timeout)
+    simple_request = urllib.request.Request(host,data=urllib.parse.urlencode(data).encode(),method=method.upper(),unverifiable=True)
+    for key,value in fake_headers.items():
+        simple_request.add_header(key,value)
+
+    for key,value in headers.items():
+        simple_request.add_header(key,value)
+
+    simple_response = opener.open(simple_request,timeout=timeout)
+
     return simple_response.url
